5bd09d8085f327a45c463f9d4fa7b7c2daafd860
Reece <Uni@192-168-1-10.tpgi.com.au>
2018-12-11 15:11:47 +1100

    added get, post and delete routes to api.js

--- api.js
+++ api.js
@@ -12,10 +12,44 @@ mongoose.connection.on('error', () => {
     console.log('failed to connect to mongod');
   });
 
-const Teams = require("./models/Team");
+const Team = require("./models/Team");
 
 const app = express();
 const port = 5000;
 
 app.use(express.json());
 
+app.get('/teams', (req, res) => {
+  Team.find({})
+    .then(doc => res.send(doc));
+})
+
+app.get('/teams/:name', (req, res) => {
+  const {name} = req.params;
+  Team.findOne({name})
+    .then(doc => res.send(doc));
+})
+
+app.post('/teams', (req, res) => {
+  // create a new team
+  const { name, location, logo, home_ground } = req.body;
+  const team = new Team({
+    name,
+    location,
+    logo,
+    home_ground
+  });
+  team.save()
+    .then(doc => res.send(doc));
+});
+
+app.delete('/team/:name', (req, res) => {
+  // remove specific team
+  const { name } = req.params;
+  Pokemon.findOneAndRemove({ name })
+    .then(doc => res.send(doc));
+});
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
